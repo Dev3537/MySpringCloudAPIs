768aa6097d75ea92b863bf0b5ef1bb8c96f55ca9
devaraj <devarajbpoliyath008@gmail.com>
2024-10-20 20:06:24 +0530

    docker compose changes and incorporating zipkin

--- currency-exchange-service/src/main/java/com/cloud/currency/currency_exchange_service/Controllers/CEController.java
+++ currency-exchange-service/src/main/java/com/cloud/currency/currency_exchange_service/Controllers/CEController.java
@@ -2,6 +2,7 @@ package com.cloud.currency.currency_exchange_service.Controllers;
 
 import com.cloud.currency.currency_exchange_service.Payload.CurrencyExchange;
 import com.cloud.currency.currency_exchange_service.Payload.repositories.CurrencyExchangeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RestController
 public class CEController {
 
@@ -24,7 +26,7 @@ public class CEController {
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public ResponseEntity<CurrencyExchange> getCurrencyExchangeRate (@PathVariable String from,
 	                                                                 @PathVariable String to) {
-
+		log.info("Called Currency Exchange for currency from {} to {}", from, to);
 		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 		if (currencyExchange == null)
 			throw new RuntimeException("No currency data found for the values supplied");
--- currency-exchange-service/src/main/java/com/cloud/currency/currency_exchange_service/Payload/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/cloud/currency/currency_exchange_service/Payload/CurrencyExchange.java
@@ -1,19 +1,27 @@
 package com.cloud.currency.currency_exchange_service.Payload;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class CurrencyExchange {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name="currency_from")
+	@Column(name = "currency_from")
 	private String from;
 
-	@Column(name="currency_to")
+	@Column(name = "currency_to")
 	private String to;
 
 	private BigDecimal conversionMultiple;
@@ -26,46 +34,6 @@ public class CurrencyExchange {
 		this.conversionMultiple = conversionMultiple;
 	}
 
-	public String getEnvironment () {
-		return environment;
-	}
-
-	public void setEnvironment (String environment) {
-		this.environment = environment;
-	}
-
-	public String getFrom () {
-		return from;
-	}
-
-	public Long getId () {
-		return id;
-	}
-
-	public String getTo () {
-		return to;
-	}
-
-	public BigDecimal getConversionMultiple () {
-		return conversionMultiple;
-	}
-
-	public void setFrom (String from) {
-		this.from = from;
-	}
-
-	public void setId (Long id) {
-		this.id = id;
-	}
-
-	public void setTo (String to) {
-		this.to = to;
-	}
-
-	public void setConversionMultiple (BigDecimal conversionMultiple) {
-		this.conversionMultiple = conversionMultiple;
-	}
-
 	public CurrencyExchange (String from, Long id, String to, BigDecimal conversionMultiple) {
 		this.from = from;
 		this.id = id;
@@ -73,6 +41,5 @@ public class CurrencyExchange {
 		this.conversionMultiple = conversionMultiple;
 	}
 
-	public CurrencyExchange () {
-	}
+
 }
